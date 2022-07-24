464c8a34b5e5091b26d7ebbb976845dce005f611
jake <palatable7@naver.com>
2022-07-24 12:54:15 +0900

    Initial Commit

---
+++ Star/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Star
+//
+//  Created by 김종권 on 2022/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Star/StarApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarApp.swift
+//  Star
+//
+//  Created by 김종권 on 2022/07/24.
+//
+
+import SwiftUI
+
+@main
+struct StarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
